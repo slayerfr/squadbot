da55209565fce4a5cf51c416f2ea8d296bf2264c
versace-corupted <84680499+versace-corupted@users.noreply.github.com>
2021-09-28 18:24:16 +0200

    mise a jour index.js
    soon

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 
+const fs = require('fs');
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
